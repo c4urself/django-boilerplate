a19335dd1a57f105efc7ca5ee53900069c82bc55
Christian Verkerk <christianverkerk@gmail.com>
2011-05-09 22:38:47 +0200

    changed 'main' to 'glue'

---
+++ example/glue/views.py
@@ -0,0 +1,21 @@
+from django.views import TemplateView
+
+class CommentView(TemplateView):
+    
+    def get(self, request, *args, **kwargs):
+        form_class = CommentForm
+        form = self.get_form(form_class)
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    # PUT is a valid HTTP verb for creating (with a known URL) or editing an
+    # object, note that browsers only support POST for now.
+    def put(self, *args, **kwargs):
+        return self.post(*args, **kwargs)
---
+++ example/serversettings/production.py
@@ -0,0 +1,7 @@
+DEBUG = False
+TEMPLATE_DEBUG = False #django-sentry suggests keeping this True
+
+SEND_BROKEN_LINK_EMAILS = True
+DEFAULT_FROM_EMAIL = 'no-reply@example.com'
+SERVER_EMAIL = 'warning@example.com'
+EMAIL_SUBJECT_PREFIX = 'Example - '
